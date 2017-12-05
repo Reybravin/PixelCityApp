8f00570e45e0e4fc8c8f9283fe47f163614f861f
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-12-05 16:47:37 +0200

    retreiving urls of photos from flickr w json

--- pixel-city/View/Constants.swift
+++ pixel-city/View/Constants.swift
@@ -11,9 +11,10 @@ import Foundation
 let apiKey = "c641a33a49050891066725c0f4996e54"
 
 func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
-    let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
-     print(url)
-    return url
+//    let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+//     print(url)
+//    return url
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
 }
 
 
